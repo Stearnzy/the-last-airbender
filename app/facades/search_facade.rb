66972b6f554f988cf35e91d65ee1b03d2f301486
Zach Stearns <zach.stearns92@gmail.com>
2020-12-11 10:34:53 -0700

    Remove moved code

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,10 +1,6 @@
 class SearchFacade
   def self.members(nation)
-    conn = Faraday.new('https://last-airbender-api.herokuapp.com')
-    
-    response = conn.get("/api/v1/characters?affiliation=#{search_conversion(nation)}&perPage=100")
-
-    json = JSON.parse(response.body, symbolize_names: true)
+    json = AirbenderService.characters(nation)
 
     @members = json.map do |member_data|
       Member.new(member_data)
@@ -12,8 +8,8 @@ class SearchFacade
   end
 
   private
-
+  
   def self.search_conversion(nation)
-    nation.split('_').map {|x| x.capitalize}.join('+')
+    nation.split('_').map { |x| x.capitalize }.join('+')
   end
 end
