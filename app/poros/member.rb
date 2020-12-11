10ae1a47ff17cccbc3e0aba8ea0ecac000bcc919
Zach Stearns <zach.stearns92@gmail.com>
2020-12-11 10:56:10 -0700

    Display enemies and allies as string instead of array

--- app/poros/member.rb
+++ app/poros/member.rb
@@ -1,11 +1,19 @@
 class Member
-  attr_reader :name, :photo, :allies, :enemies, :affiliation
+  attr_reader :name, :photo, :affiliation
 
   def initialize(member_data)
     @name = member_data[:name]
+    @data = member_data
     @photo = member_data[:photoUrl] ? member_data[:photoUrl] : "Not Pictured"
-    @allies = member_data[:allies] ? member_data[:allies] : "No Allies"
     @enemies = member_data[:enemies] ? member_data[:enemies] : "No Enemies"
     @affiliation = member_data[:affiliation] ? member_data[:affiliation] : "No Affiliation"
+  end
+
+  def allies
+    @data[:allies].join(', ')
+  end
+
+  def enemies
+    @data[:enemies].join(', ')
   end
 end
