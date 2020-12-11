b02159fb58b55e5f8662cf15cc5564d887ff97ff
Zach Stearns <zach.stearns92@gmail.com>
2020-12-11 09:44:57 -0700

    Write out response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   def index
-    
+    search = params[:nation]
+
+    conn = Faraday.new('https://last-airbender-api.herokuapp.com')
+
+    response = conn.get("/api/v1/characters?affiliation=Fire+Nation&perPage=100")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @members = json
+
+    require 'pry'; binding.pry
   end
 end
