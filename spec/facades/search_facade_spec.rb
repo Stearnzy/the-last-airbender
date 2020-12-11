13c3a0add6890b717b2c549c46152ac13abb90a9
Zach Stearns <zach.stearns92@gmail.com>
2020-12-11 10:19:16 -0700

    Tested search conversion method

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -4,4 +4,8 @@ describe 'Search Facade Testing' do
   it '#members(nation)' do
     expect(SearchFacade.members('fire_nation')).to be_an Array
   end
+
+  it '#search_conversion(nation)' do
+    expect(SearchFacade.search_conversion('fire_nation')).to eq('Fire+Nation')
+  end
 end
