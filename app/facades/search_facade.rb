33df95f4a6ae89ea4355e46c60f14c1bc42fd747
Zach Stearns <zach.stearns92@gmail.com>
2020-12-11 10:18:14 -0700

    Split search_conversion method to helper

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,15 +1,19 @@
 class SearchFacade
   def self.members(nation)
-    search_conversion = nation.split('_').map {|x| x.capitalize}.join('+')
-    
     conn = Faraday.new('https://last-airbender-api.herokuapp.com')
     
-    response = conn.get("/api/v1/characters?affiliation=#{search_conversion}&perPage=100")
+    response = conn.get("/api/v1/characters?affiliation=#{search_conversion(nation)}&perPage=100")
 
     json = JSON.parse(response.body, symbolize_names: true)
 
     @members = json.map do |member_data|
       Member.new(member_data)
     end
+  end
+
+  private
+
+  def self.search_conversion(nation)
+    nation.split('_').map {|x| x.capitalize}.join('+')
   end
 end
